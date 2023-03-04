ef92dc9d112f77529a21e655c61dd2ccd75676a1
Nikita <405663260nikita@gmail.com>
2023-03-04 11:36:26 +0300

    some changes in views and api

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -1,7 +1,7 @@
 from flask import request, abort, current_app
 import jwt
 
-from constants import JWT_SECRET, JWT_ALGO
+
 from container import user_service
 
 
@@ -48,7 +48,7 @@ def user_required(func):
         data = request.headers['Authorization']
         token = data.split('Bearer ')[-1]
         try:
-            user_data = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGO])
+            user_data = jwt.decode(token, current_app.config["JWT_SECRET"], algorithms=[current_app.config["JWT_ALGO"]])
             user = user_service.get_by_username(user_data['username'])
         except Exception as e:
             abort(400)

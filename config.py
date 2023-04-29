5ec52a4bc49132447a45b84040c01e3a6b567888
Nikita Bochkarev <405663260nikita@gmail.com>
2023-04-29 21:00:20 +0300

    added docker-compose

--- config.py
+++ config.py
@@ -6,12 +6,7 @@ from typing import Type
 class Config(object):
     DEBUG = True
     SECRET_HERE = '249y823r9v8238r9u'
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT', 5432)
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = "postgresql://lib_4:lib_4@pg/lib_4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PWD_HASH_SALT = b'secret here'
     PWD_HASH_ITERATIONS = 100_000

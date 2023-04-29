36319b88d5ae3303ee01b43fbaa2d4d346634bec
Nikita Bochkarev <405663260nikita@gmail.com>
2023-04-29 12:04:47 +0300

    finished project

--- tools/security.py
+++ tools/security.py
@@ -15,11 +15,20 @@ def _create_hash(password: str) -> bytes:
     return hash_password
 
 
-def get_hash(password: str) -> bytes:
-    return base64.b64encode(_create_hash(password))
+def get_hash(password: str):
+    # return base64.b64encode(_create_hash(password))
+    print('________-')
+    p = _create_hash(password)
+    print(p)
+    p_2 = base64.b64encode(p)
+    print(p_2)
+    print('________')
+    return p_2.decode()
 
 
 def compare_password(right_password: str, other_password: str) -> bool:
-    decode_right_password = base64.b64decode(right_password)
+    decode_right_password = base64.b64decode(right_password.encode())
+    print(decode_right_password)
     hash_other_password = _create_hash(other_password)
+    print(hash_other_password)
     return hmac.compare_digest(decode_right_password, hash_other_password)

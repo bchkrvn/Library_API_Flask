d1c5491e76b7a7be0ff52bd02278c956f1a4d2d4
Nikita Bochkarev <405663260nikita@gmail.com>
2023-08-19 11:15:27 +0300

    add description in security and add DS_Store files to .gitignore

--- application/tools/security.py
+++ application/tools/security.py
@@ -6,6 +6,7 @@ from flask import current_app
 
 
 def _create_hash(password: str) -> bytes:
+    """make hash from password"""
     hash_password = hashlib.pbkdf2_hmac(
         current_app.config["HASH_NAME"],
         password.encode('utf-8'),
@@ -16,10 +17,12 @@ def _create_hash(password: str) -> bytes:
 
 
 def get_hash(password: str) -> str:
+    """get hash from password"""
     return base64.b64encode(_create_hash(password)).decode()
 
 
 def compare_password(right_password: str, other_password: str) -> bool:
+    """compare password's hash"""
     decode_right_password = base64.b64decode(right_password.encode())
     hash_other_password = _create_hash(other_password)
     return hmac.compare_digest(decode_right_password, hash_other_password)
